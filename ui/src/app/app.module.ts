eaedc02e33fa521831e59c2a01ceb980c4cab758
Juan Valentin <j.vlt.mtz@gmail.com>
2018-09-01 17:48:31 -0500

    Modal genrico

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -2,11 +2,13 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {AppComponent} from './app.component';
 import {AppRoutingModule} from './app-routing.module';
+import { ModalComponent } from './components/modal/modal.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ModalComponent
   ],
   imports: [
     BrowserModule,
