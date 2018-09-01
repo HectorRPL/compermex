df0fc366ab79e5f0658dfd8948051744a93a6200
Juan Valentin <j.vlt.mtz@gmail.com>
2018-09-01 17:55:41 -0500

    Se quitan comillas dobles en el import

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -2,7 +2,7 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {AppComponent} from './app.component';
 import {AppRoutingModule} from './app-routing.module';
-import {NgbModule} from "@ng-bootstrap/ng-bootstrap";
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { ModalComponent } from './components/modal/modal.component';
 
 
