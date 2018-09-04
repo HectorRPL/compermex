571a3a41c319497f8bbe96d2daef442520b37bd4
Juan Valentin <j.vlt.mtz@gmail.com>
2018-09-03 23:33:02 -0500

    prueba de autocomplete

--- ui/src/app/layout/components/searches/suppliers-search/suppliers-search.component.ts
+++ ui/src/app/layout/components/searches/suppliers-search/suppliers-search.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {Observable} from "rxjs";
+import {catchError, debounceTime, distinctUntilChanged, switchMap, tap} from "rxjs/operators";
+import {of} from 'rxjs/observable/of';
+
 
 @Component({
   selector: 'app-suppliers-search',
@@ -7,9 +11,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SuppliersSearchComponent implements OnInit {
 
+  model: any;
+  searching = false;
+  searchFailed = false;
+
   constructor() { }
 
   ngOnInit() {
   }
+
+
+  search(text$: Observable<string>) {
+    text$.pipe(
+      debounceTime(300),
+      distinctUntilChanged(),
+      tap(() => this.searching = true),
+      switchMap(term =>
+        this._service.search(term).pipe(
+          tap(() => this.searchFailed = false),
+          catchError(() => {
+            this.searchFailed = true;
+            return of([]);
+          }))
+      ),
+      tap(() => this.searching = false)
+    )
+  }
+
+
 
 }
