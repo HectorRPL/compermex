fa32299a54af6eab87542d2d719935ecf849744a
hEctor <hector.kaizen@gmail.com>
2018-10-02 21:23:11 -0500

    Avance

---
+++ ui/src/app/models/cardboard/cardboard.mock.ts
@@ -0,0 +1,22 @@
+import {Cardboard} from "./cardboard.model";
+
+export const CARDBOARDS: Cardboard[] = [
+  {
+    "_id": {$oid: "5b9423b29823b52e3232a7b1"},
+    "type": "Corrugado Sencillo",
+    "color": "KRAFT",
+    "strength": "S/G"
+  },
+  {
+    "_id": {$oid: "5b9423b29823b52e3232a7b2"},
+    "type": "Doble Corrugado",
+    "color": "L. BCO.",
+    "strength": "7 KGS."
+  },
+  {
+    "_id": {$oid: "5b9423b29823b52e3232a7b3"},
+    "type": "Micro Corrugado",
+    "color": "L. BCO.",
+    "strength": "9 KGS."
+  }
+];
---
+++ ui/src/app/models/cardboard/cardboard.model.ts
@@ -0,0 +1,8 @@
+import {ObjectId} from "../object-id.model";
+
+export class Cardboard {
+  _id:      ObjectId;
+  type:       string;
+  color:      string;
+  strength:   string;
+}
