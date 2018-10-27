c8a7d29c40962ae65c92dae4568760c29320e38d
LAPTOP-INF2C1Q9\User <hefloresv@outlook.com>
2018-10-26 23:17:49 -0500

    Quita código que no se está usando

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -3,12 +3,9 @@ import {NgModule} from '@angular/core';
 import {AppComponent} from './app.component';
 import {AppRoutingModule} from './app-routing.module';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
-import {ModalComponent} from './components/modal/modal.component';
 import {MessagesService} from './services/message/messages.service';
 import {HttpErrorHandlerService} from './services/http-error-handler.service';
 import {HttpClientModule} from '@angular/common/http';
-import {HttpClientInMemoryWebApiModule} from 'angular-in-memory-web-api';
-import {InMemoryDataService} from './services/in-memory-data.service';
 import {httpInterceptorProviders} from './http-interceptors';
 import {CookieModule} from 'ngx-cookie';
 import {Ng2UiAuthModule, StorageType} from 'ng2-ui-auth';
@@ -16,8 +13,7 @@ import {Ng2UiAuthModule, StorageType} from 'ng2-ui-auth';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ModalComponent
+    AppComponent
   ],
   imports: [
     HttpClientModule,
--- ui/src/app/layout/layout.module.ts
+++ ui/src/app/layout/layout.module.ts
@@ -1,11 +1,8 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {NgbDropdownModule} from '@ng-bootstrap/ng-bootstrap';
-
 import {LayoutRoutingModule} from './layout-routing.module';
 import {LayoutComponent} from './layout.component';
-import {SidebarComponent} from './components/sidebar/sidebar.component';
-import {HeaderComponent} from './components/header/header.component';
 import {SupplierService} from '../services/suppliers/supplier.service';
 import {CompanyService} from '../services/company/company.service';
 import {CardboardService} from '../services/cardboard/cardboard.service';
