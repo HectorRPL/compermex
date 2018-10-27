439009ec1169e11d1c1d46023085dcfebc37c681
LAPTOP-INF2C1Q9\User <hefloresv@outlook.com>
2018-10-27 16:26:35 -0500

    Se corrigen unos imports

--- ui/src/app/layout/suppliers/suppliers-routing.module.ts
+++ ui/src/app/layout/suppliers/suppliers-routing.module.ts
@@ -2,7 +2,7 @@ import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 import {SuppliersComponent} from './components/suppliers/suppliers.component';
 import {ListSuppliersComponent} from './views/list-suppliers/list-suppliers.component';
-import {AddEmployeeComponent} from "../employees/views/add-employee/add-employee.component";
+import {AddSupplierComponent} from './views/add-supplier/add-supplier.component';
 
 
 const routes: Routes = [
@@ -14,7 +14,7 @@ const routes: Routes = [
         path: 'list', component: ListSuppliersComponent
       },
       {
-        path: 'add', component: AddEmployeeComponent
+        path: 'add', component: AddSupplierComponent
       }
     ]
   }
