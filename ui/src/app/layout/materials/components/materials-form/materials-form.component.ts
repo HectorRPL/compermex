bef40e8907e8b3bb5d6c99e4f293632affe2668e
hector <hector.kaizen@gmail.com>
2018-11-03 00:25:18 -0600

    Se crea la forma (genérica) para los materiales

--- ui/src/app/layout/materials/components/materials-form/materials-form.component.ts
+++ ui/src/app/layout/materials/components/materials-form/materials-form.component.ts
@@ -9,17 +9,12 @@ import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 })
 export class MaterialsFormComponent implements OnInit {
 
-  minSize: number;
-  maxSize: number;
-
   public material: Material;
   materialsForm: FormGroup;
 
   constructor(private formBuilder: FormBuilder) {
 
     this.material = new Material();
-    this.minSize = 1;
-    this.maxSize = 10;
 
   }
 
@@ -31,150 +26,14 @@ export class MaterialsFormComponent implements OnInit {
 
   createMaterialsForm() {
     this.materialsForm = this.formBuilder.group({
-      // PANEL ARTÍCULO
-      'cia': new FormControl(this.material.cia, [
+      'description': new FormControl(this.material.description, [
         Validators.required
-      ]),
-      'key': new FormControl(this.material.key, [
-        Validators.required
-      ]),
-      'ourKey': new FormControl(this.material.ourKey, [
-        Validators.required
-      ]),
-      'type': new FormControl(this.material.type, [
-        Validators.required
-      ]),
-      'client': new FormControl(this.material.client, [
-        Validators.required
-      ]),
-      'businessName': new FormControl(this.material.businessName, [
-        Validators.required
-      ]),
-      'seller': new FormControl(this.material.seller, [
-        Validators.required
-      ]),
-
-      // <!--PANEL MEDIDAS-->
-      'long': new FormControl(this.material.long, [
-        Validators.required,
-        Validators.min(this.minSize),
-        Validators.max(this.maxSize)
-      ]),
-      'width': new FormControl(this.material.width, [
-        Validators.required,
-        Validators.min(this.minSize),
-        Validators.max(this.maxSize)
-      ]),
-      'height': new FormControl(this.material.height, [
-        Validators.required,
-        Validators.min(this.minSize),
-        Validators.max(this.maxSize)
-      ]),
-      'slots': new FormControl(this.material.slots, [
-        Validators.required,
-        Validators.min(this.minSize),
-        Validators.max(this.maxSize)
-      ]),
-
-      // <!--PANEL ESPEFIFICACIONES DE MATERIAL-->
-      'purchase': new FormControl(this.material.purchase, [
-        Validators.required
-      ]),
-      'sale': new FormControl(this.material.sale, [
-        Validators.required
-      ]),
-      'sellerPrice': new FormControl(this.material.sellerPrice, [
-        Validators.required
-      ]),
-      'maxPercentage': new FormControl(this.material.maxPercentage, [
-        Validators.required
-      ]),
-      'minPercentage': new FormControl(this.material.minPercentage, [
-        Validators.required
-      ]),
-      'certificate': new FormControl(this.material.certificate, [
-        Validators.required
-      ]),
-      'flat': new FormControl(this.material.flat, [
-        Validators.required
-      ]),
-      'observations': new FormControl(this.material.observations, [
-        Validators.required,
-        Validators.minLength(this.minSize),
-        Validators.maxLength(this.maxSize)
-      ]),
-      'company': new FormControl(this.material.company, [
-        Validators.required,
-        Validators.minLength(this.minSize),
-        Validators.maxLength(this.maxSize)
       ])
     });
   }
 
-  // PANEL ARTÍCULO
-  get cia() {
-    return this.materialsForm.get('cia');
-  }
-  get key() {
-    return this.materialsForm.get('key');
-  }
-  get ourKey() {
-    return this.materialsForm.get('ourKey');
-  }
-  get type() {
-    return this.materialsForm.get('type');
-  }
-  get client() {
-    return this.materialsForm.get('client');
-  }
-  get businessName() {
-    return this.materialsForm.get('businessName');
-  }
-  get seller() {
-    return this.materialsForm.get('seller');
-  }
-  get company() {
-    return this.materialsForm.get('company');
-  }
-
-  // PANEL MEDIDAS
-  get long() {
-    return this.materialsForm.get('long');
-  }
-  get width() {
-    return this.materialsForm.get('width');
-  }
-  get height() {
-    return this.materialsForm.get('height');
-  }
-  get slots() {
-    return this.materialsForm.get('slots');
-  }
-
-  // PANEL ESPEFIFICACIÓN DE MATERIAL
-  get purchase() {
-    return this.materialsForm.get('purchase');
-  }
-  get sale() {
-    return this.materialsForm.get('sale');
-  }
-  get sellerPrice() {
-    return this.materialsForm.get('sellerPrice');
-  }
-  get maxPercentage() {
-    return this.materialsForm.get('maxPercentage');
-  }
-  get minPercentage() {
-    return this.materialsForm.get('minPercentage');
-  }
-  get certificate() {
-    return this.materialsForm.get('certificate');
-  }
-  get flat() {
-    return this.materialsForm.get('flat');
-  }
-  get observations() {
-    return this.materialsForm.get('observations');
+  get description() {
+    return this.materialsForm.get('description');
   }
 
   materialAction() {
--- ui/src/app/layout/materials/models/material.model.ts
+++ ui/src/app/layout/materials/models/material.model.ts
@@ -1,29 +1,3 @@
 export class Material {
-  // PANEL ARTÍCULO
-  key: string;
-  ourKey: string;
-  type: number;
-  cia: number;
-  client: number;
-  businessName: string;
-  seller: string;
-  company: string;
-
-
-
-  // <!--PANEL MEDIDAS-->
-  long: number; // Largo
-  width: number; // Ancho
-  height: number; // Alto
-  slots: number; // Ranuras
-
-  // <!--PANEL ESPEFIFICACIONES DE MATERIAL-->
-  purchase: number;
-  sale: number;
-  sellerPrice: number;
-  maxPercentage: number;
-  minPercentage: number;
-  certificate: number;
-  flat: number;
-  observations: string;
+  description: string;
 }
