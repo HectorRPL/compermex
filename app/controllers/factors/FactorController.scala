1f03b04e831fd7c0e4fe5534ee0f176a2fe6d824
Juan Valentin <j.vlt.mtz@gmail.com>
2019-03-10 19:03:53 -0600

    metodo para generar query

--- app/controllers/factors/FactorController.scala
+++ app/controllers/factors/FactorController.scala
@@ -1,12 +1,11 @@
 package controllers.factors
 
 import javax.inject.Inject
-
 import models.Pagination
 import models.factor.Factor
 import myservices.factors.FactorsService
 import play.api.i18n.{I18nSupport, Messages}
-import play.api.libs.json.Json
+import play.api.libs.json.{JsObject, Json}
 import play.api.mvc.{AbstractController, ControllerComponents}
 
 import scala.concurrent.{ExecutionContext, Future}
@@ -20,7 +19,39 @@ class FactorController @Inject()(
 
   def factors(name: String, curPage: Int, pageSize: Int) = Action.async {
 
-    val query = Json.obj("$or" ->
+    val query = buildDescriptionQuery(name)
+
+    val sort = Json.obj("type.description" -> -1)
+    val pag = Pagination.fromPages(curPage, pageSize)
+
+    factorsService.getAll(query, sort, pag).map { factor =>
+      Ok(Json.toJson(factor))
+    }
+  }
+
+  def count(name: String) = Action.async {
+   val query = buildDescriptionQuery(name)
+
+    factorsService.count(query).map { result =>
+      Ok(Json.toJson(result))
+    }
+  }
+
+
+  def save() = Action.async(parse.json) { implicit request =>
+    request.body.validate[Factor]
+      .map { data =>
+        factorsService.save(data).map { result =>
+          Ok(Json.toJson(result))
+        }
+      }.recoverTotal {
+      case error =>
+        Future.successful(BadRequest(Json.obj("message" -> Messages("invalid.data"))))
+    }
+  }
+
+  def buildDescriptionQuery(name: String): JsObject = {
+    Json.obj("$or" ->
       Json.arr(
         Json.obj(
           "typeMaterial.description" ->
@@ -37,34 +68,6 @@ class FactorController @Inject()(
         )
       )
     )
-
-    val sort = Json.obj("type.description" -> -1)
-    val pag = Pagination.fromPages(curPage, pageSize)
-
-    factorsService.getAll(query, sort, pag).map { factor =>
-      Ok(Json.toJson(factor))
-    }
-  }
-
-  def count(name: String) = Action.async {
-    val query = Json.obj(
-      "factor1" -> Json.obj("$regex" -> name, "$options" -> "i"))
-
-    factorsService.count(query).map { result =>
-      Ok(Json.toJson(result))
-    }
-  }
-
-  def save() = Action.async(parse.json) { implicit request =>
-    request.body.validate[Factor]
-      .map { data =>
-        factorsService.save(data).map { result =>
-          Ok(Json.toJson(result))
-        }
-      }.recoverTotal {
-      case error =>
-        Future.successful(BadRequest(Json.obj("message" -> Messages("invalid.data"))))
-    }
   }
 }
 
